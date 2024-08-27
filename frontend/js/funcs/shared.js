a22031c46b8261babe1f16f5ca046ea353dec28e
AliFakhrodin <alifakhrodin16@gmail.com>
2024-08-27 07:00:10 +0330

    get navbar menu from db

--- frontend/js/funcs/shared.js
+++ frontend/js/funcs/shared.js
@@ -40,21 +40,259 @@ const renderTopbarMenus = async () => {
 }
 
 const getAndShowAllCourses = async () => {
-     const res = axios({ url: 'http://localhost:4000/v1/courses', method: 'get' })
+     const coursesContainer = document.querySelector('.courses-content .row')
+     const res = await axios({ url: 'http://localhost:4000/v1/courses', method: 'get' })
+
+     res.data.slice(0, 6).map(course => {
+          coursesContainer.insertAdjacentHTML('beforeend',
+               `<div class="col-4">
+          <div class="course-box d-flex flex-xl-column rounded-4 mt-5 overflow-hidden">
+               <a href="#">
+                    <img src="http://localhost:4000/courses/covers/${course.cover}" alt="fareelancer-image"
+                         class="course-box__img w-100">
+               </a>
+
+               <div class="course-box__main px-4">
+                    <a href="#" class="course-box__title d-block pt-4">
+                         ${course.name}
+                    </a>
+                    <div
+                         class="course-box__rating-teacher d-flex justify-content-between align-items-center py-3">
+                         <div class="course-box__teacher d-flex align-items-center gap-2">
+                              <i class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
+                              <a href="#" class="course-box__teacher-link">${course.creator}</a>
+                         </div>
+                         <div class="course-box__rating">
+                              ${Array(5 - course.courseAverageScore).fill(0).map(score =>
+                    `<img src="./images/svgs/star.svg" alt="rating" class="course-box__star"></img>`
+               ).join('')
+               }
+                              ${Array(course.courseAverageScore).fill(0).map(score =>
+                    `<img src="./images/svgs/star_fill.svg" alt="rating" class="course-box__star"></img>`
+               ).join('')
+               }
+                    </div>
+                    </div>
+                    <div class="course-box_status d-flex justify-content-between">
+                         <div class="course-box__user">
+                              <i class="fas fa-users course-box__users-icon"></i>
+                              <span class="course-box__users-text">${course.registers}</span>
+                         </div>
+                         <span class="course-box__price">${course.price ? course.price.toLocaleString() : 'رایگان'}</span>
+                    </div>
+                    </div>
+
+                    <div class="course-box__footer mt-3">
+                         <a href="#"
+                              class="course-box__footer-link d-flex align-items-center gap-2 justify-content-center py-3">
+                              مشاهده اطلاعات
+                              <i class="fas fa-arrow-left course-box__footer-icon"></i>
+                         </a>
+                    </div>
+               </div>
+     </div>`
+          )
+     })
 
      return res
 }
 
 const getAndShowPopularCourses = async () => {
-     const res = axios({url:`http://localhost:4000/v1/courses/popular`, method: 'get'})
-     
+     const popularContainer = document.querySelector('.popular__swiper .swiper-wrapper')
+     const res = await axios({ url: `http://localhost:4000/v1/courses/popular`, method: 'get' })
+
+     res.data.forEach(course => {
+          popularContainer.insertAdjacentHTML('beforeend', `
+               <div class="swiper-slide">
+                                   <div class="course-box d-flex flex-xl-column rounded-4 mt-5 overflow-hidden">
+                                        <a href="#">
+                                             <img src="http://localhost:4000/courses/covers/${course.cover}" alt="fareelancer-image"
+                                                  class="course-box__img w-100">
+                                        </a>
+
+                                        <div class="course-box__main px-4">
+                                             <a href="#" class="course-box__title d-block pt-4">
+                                                  ${course.name}
+                                             </a>
+
+                                             <div
+                                                  class="course-box__rating-teacher d-flex justify-content-between align-items-center py-3">
+                                                  <div class="course-box__teacher d-flex align-items-center gap-2">
+                                                       <i
+                                                            class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
+                                                       <a href="#" class="course-box__teacher-link">${course.creator}</a>
+                                                  </div>
+
+                                                  <div class="course-box__rating">
+                                                  ${Array(5 - course.courseAverageScore).fill(0).map(() =>
+               `<img src="./images/svgs/star.svg" alt="rating"
+                                                                 class="course-box__star">`
+          ).join('')
+               }
+                                                       ${Array(course.courseAverageScore).fill(0).map(() =>
+                    `<img src="./images/svgs/star_fill.svg" alt="rating"
+                                                                      class="course-box__star">`
+               ).join('')
+               }
+                                                       
+                                                  </div>
+                                             </div>
+
+                                             <div class="course-box_status d-flex justify-content-between">
+                                                  <div class="course-box__user">
+                                                       <i class="fas fa-users course-box__users-icon"></i>
+                                                       <span class="course-box__users-text">${course.registers}</span>
+                                                  </div>
+                                                  <span class="course-box__price">${course.price ? course.price.toLocaleString() : "رایگان"}</span>
+                                             </div>
+                                        </div>
+
+                                        <div class="course-box__footer mt-3">
+                                             <a href="#"
+                                                  class="course-box__footer-link d-flex align-items-center gap-2 justify-content-center py-3">
+                                                  مشاهده اطلاعات
+                                                  <i class="fas fa-arrow-left course-box__footer-icon"></i>
+                                             </a>
+                                        </div>
+                                   </div>
+                              </div>`)
+     })
+
      return res
 }
 
 const getAndShowPresellCourses = async () => {
-     const res = axios({url:`http://localhost:4000/v1/courses/presell`})
-     
+     const presellContainer = document.querySelector('.presell__swiper .swiper-wrapper')
+     const res = await axios({ url: `http://localhost:4000/v1/courses/presell` })
+
+     res.data.forEach(course => {
+          presellContainer.insertAdjacentHTML('beforeend', `
+                    <div class="swiper-slide">
+                         <div class="course-box d-flex flex-xl-column rounded-4 mt-5 overflow-hidden">
+                              <a href="#">
+                                   <img src="http://localhost:4000/courses/covers/${course.cover}" alt="fareelancer-image"
+                                        class="course-box__img w-100">
+                              </a>
+
+                              <div class="course-box__main px-4">
+                                   <a href="#" class="course-box__title d-block pt-4">
+                                        ${course.name}
+                                   </a>
+
+                                   <div
+                                        class="course-box__rating-teacher d-flex justify-content-between align-items-center py-3">
+                                        <div class="course-box__teacher d-flex align-items-center gap-2">
+                                             <i
+                                                  class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
+                                             <a href="#" class="course-box__teacher-link">${course.creator}</a>
+                                        </div>
+
+                                        <div class="course-box__rating">
+                                        ${Array(5 - course.courseAverageScore).fill(0).map(() =>
+               `<img src="./images/svgs/star.svg" alt="rating"
+                                                       class="course-box__star">`
+          ).join('')
+               }
+                                             ${Array(course.courseAverageScore).fill(0).map(() =>
+                    `<img src="./images/svgs/star_fill.svg" alt="rating"
+                                                            class="course-box__star">`
+               ).join('')
+               }            
+                                        </div>
+                                   </div>
+
+                                   <div class="course-box_status d-flex justify-content-between">
+                                        <div class="course-box__user">
+                                             <i class="fas fa-users course-box__users-icon"></i>
+                                             <span class="course-box__users-text">${course.registers}</span>
+                                        </div>
+                                        <span class="course-box__price">${course.price ? course.price.toLocaleString() : "رایگان"}</span>
+                                   </div>
+                              </div>
+
+                              <div class="course-box__footer mt-3">
+                                   <a href="#"
+                                        class="course-box__footer-link d-flex align-items-center gap-2 justify-content-center py-3">
+                                        مشاهده اطلاعات
+                                        <i class="fas fa-arrow-left course-box__footer-icon"></i>
+                                   </a>
+                              </div>
+                         </div>
+                    </div>`)
+     });
+
      return res
 }
 
-export { ShowUserNameInNavbar, renderTopbarMenus, getAndShowAllCourses, getAndShowPopularCourses, getAndShowPresellCourses }
+const getAndShowArticles = async () => {
+     const artileContainer = document.querySelector('.article__content .row')
+     const res = await axios({ url: `http://localhost:4000/v1/articles` })
+
+     res.data.slice(0, 3).forEach(article => {
+          artileContainer.insertAdjacentHTML('beforeend', `
+               <div class="col-4 my-5">
+                         <div class="article-card rounded-4 overflow-hidden pb-4">
+                              <div class="article-card__header">
+                                   <a href="#" class="d-flex justify-content-center">
+                                        <img src="http://localhost:4000/courses/covers/${article.cover}" class="article-card__img w-100"
+                                             alt="Article-Cover">
+                                   </a>
+                              </div>
+
+                              <div class="article-card__content px-4 pb-2 mt-4">
+                                   <a href="#" class="article-card__link fw-bold fs-3">
+                                        ${article.title}
+                                   </a>
+                                   <p class="article-card__text fs-5">
+                                        ${article.description}
+                                   </p>
+                                   <a class="article-card__btn px-3 py-1 rounded-3 fs-4 d-inline">
+                                        <span>بیشتر بخوانید</span>
+                                   </a>
+                              </div>
+                         </div>
+                    </div>
+               `)
+     });
+
+     return res
+}
+
+const getAndShowNavbarMenus = async () => {
+     const artileContainer = document.querySelector('.main-header__menu')
+     const res = await axios({ url: `http://localhost:4000/v1/menus` })
+
+     res.data.forEach(item => {
+          artileContainer.insertAdjacentHTML('beforeend',
+               `<li class="main-header__item">
+                    <a href="#" class="main-header__link">${item.title}</a>
+
+                    ${item.submenus.length !== 0 ?
+                    `<i class="fas fa-angle-down"></i>
+                         <ul class="main-header__dropdown">
+                         ${item.submenus.map(submenu => (
+                                   `<li class="main-header__dropdown-item">
+                                        <a href="#" class="main-header__dropdown-link">
+                                             ${submenu.title}
+                                        </a>
+                                   </li>`
+                              )).join('')}
+                         </ul>` : ''
+                    }
+
+               </li>`)
+     })
+     
+     console.log(res);
+     return res
+}
+
+export {
+     ShowUserNameInNavbar,
+     renderTopbarMenus,
+     getAndShowAllCourses,
+     getAndShowPopularCourses,
+     getAndShowPresellCourses,
+     getAndShowArticles,
+     getAndShowNavbarMenus,
+}
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,10 @@
-import { getAndShowAllCourses, getAndShowPopularCourses, getAndShowPresellCourses } from './funcs/shared.js';
+import {
+     getAndShowAllCourses,
+     getAndShowPopularCourses,
+     getAndShowPresellCourses,
+     getAndShowArticles,
+     getAndShowNavbarMenus,
+} from './funcs/shared.js';
 
 const $ = document
 const landingTitle = $.querySelector('.landing-title')
@@ -8,11 +14,6 @@ let typewriterCounter = 0
 const landingTimeCount = $.querySelector('#time-count')
 const landingCourseCount = $.querySelector('#courses-count')
 const landingUsersCount = $.querySelector('#users-count')
-
-const coursesContainer = document.querySelector('.courses-content .row')
-const popularContainer = document.querySelector('.popular__swiper .swiper-wrapper')
-const presellContainer = document.querySelector('.presell__swiper .swiper-wrapper')
-
 
 function makeCounter(max, elem, step) {
      let counter = 0;
@@ -43,178 +44,8 @@ window.addEventListener('load', () => {
      makeCounter(68_059, landingUsersCount, 500)
 
      getAndShowAllCourses()
-          .then(res => {
-               res.data.slice(0, 6).map(course => {
-                    coursesContainer.insertAdjacentHTML('beforeend',
-                         `<div class="col-4">
-                    <div class="course-box d-flex flex-xl-column rounded-4 mt-5 overflow-hidden">
-                         <a href="#">
-                              <img src="http://localhost:4000/courses/covers/${course.cover}" alt="fareelancer-image"
-                                   class="course-box__img w-100">
-                         </a>
-     
-                         <div class="course-box__main px-4">
-                              <a href="#" class="course-box__title d-block pt-4">
-                                   ${course.name}
-                              </a>
-                              <div
-                                   class="course-box__rating-teacher d-flex justify-content-between align-items-center py-3">
-                                   <div class="course-box__teacher d-flex align-items-center gap-2">
-                                        <i class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
-                                        <a href="#" class="course-box__teacher-link">${course.creator}</a>
-                                   </div>
-                                   <div class="course-box__rating">
-                                        ${Array(5 - course.courseAverageScore).fill(0).map(score =>
-                              `<img src="./images/svgs/star.svg" alt="rating" class="course-box__star"></img>`
-                         ).join('')
-                         }
-                                        ${Array(course.courseAverageScore).fill(0).map(score =>
-                              `<img src="./images/svgs/star_fill.svg" alt="rating" class="course-box__star"></img>`
-                         ).join('')
-                         }
-                              </div>
-                              </div>
-                              <div class="course-box_status d-flex justify-content-between">
-                                   <div class="course-box__user">
-                                        <i class="fas fa-users course-box__users-icon"></i>
-                                        <span class="course-box__users-text">${course.registers}</span>
-                                   </div>
-                                   <span class="course-box__price">${course.price ? course.price.toLocaleString() : 'رایگان'}</span>
-                              </div>
-                              </div>
-     
-                              <div class="course-box__footer mt-3">
-                                   <a href="#"
-                                        class="course-box__footer-link d-flex align-items-center gap-2 justify-content-center py-3">
-                                        مشاهده اطلاعات
-                                        <i class="fas fa-arrow-left course-box__footer-icon"></i>
-                                   </a>
-                              </div>
-                         </div>
-               </div>`
-                    )
-               })
-          })
-
      getAndShowPopularCourses()
-          .then(res => {
-               res.data.forEach(course => {
-                    popularContainer.insertAdjacentHTML('beforeend', `
-                         <div class="swiper-slide">
-                                             <div class="course-box d-flex flex-xl-column rounded-4 mt-5 overflow-hidden">
-                                                  <a href="#">
-                                                       <img src="http://localhost:4000/courses/covers/${course.cover}" alt="fareelancer-image"
-                                                            class="course-box__img w-100">
-                                                  </a>
-     
-                                                  <div class="course-box__main px-4">
-                                                       <a href="#" class="course-box__title d-block pt-4">
-                                                            ${course.name}
-                                                       </a>
-     
-                                                       <div
-                                                            class="course-box__rating-teacher d-flex justify-content-between align-items-center py-3">
-                                                            <div class="course-box__teacher d-flex align-items-center gap-2">
-                                                                 <i
-                                                                      class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
-                                                                 <a href="#" class="course-box__teacher-link">${course.creator}</a>
-                                                            </div>
-     
-                                                            <div class="course-box__rating">
-                                                            ${Array(5 - course.courseAverageScore).fill(0).map(() =>
-                         `<img src="./images/svgs/star.svg" alt="rating"
-                                                                           class="course-box__star">`
-                    ).join('')
-                         }
-                                                                 ${Array(course.courseAverageScore).fill(0).map(() =>
-                              `<img src="./images/svgs/star_fill.svg" alt="rating"
-                                                                                class="course-box__star">`
-                         ).join('')
-                         }
-                                                                 
-                                                            </div>
-                                                       </div>
-     
-                                                       <div class="course-box_status d-flex justify-content-between">
-                                                            <div class="course-box__user">
-                                                                 <i class="fas fa-users course-box__users-icon"></i>
-                                                                 <span class="course-box__users-text">${course.registers}</span>
-                                                            </div>
-                                                            <span class="course-box__price">${course.price ? course.price.toLocaleString() : "رایگان"}</span>
-                                                       </div>
-                                                  </div>
-     
-                                                  <div class="course-box__footer mt-3">
-                                                       <a href="#"
-                                                            class="course-box__footer-link d-flex align-items-center gap-2 justify-content-center py-3">
-                                                            مشاهده اطلاعات
-                                                            <i class="fas fa-arrow-left course-box__footer-icon"></i>
-                                                       </a>
-                                                  </div>
-                                             </div>
-                                        </div>`)
-               })
-          })
-
      getAndShowPresellCourses()
-          .then(res => {
-               console.log(res)
-               console.log(res.data)
-
-               res.data.forEach(course => {
-                    presellContainer.insertAdjacentHTML('beforeend', `
-                              <div class="swiper-slide">
-                                   <div class="course-box d-flex flex-xl-column rounded-4 mt-5 overflow-hidden">
-                                        <a href="#">
-                                             <img src="http://localhost:4000/courses/covers/${course.cover}" alt="fareelancer-image"
-                                                  class="course-box__img w-100">
-                                        </a>
-
-                                        <div class="course-box__main px-4">
-                                             <a href="#" class="course-box__title d-block pt-4">
-                                                  ${course.name}
-                                             </a>
-
-                                             <div
-                                                  class="course-box__rating-teacher d-flex justify-content-between align-items-center py-3">
-                                                  <div class="course-box__teacher d-flex align-items-center gap-2">
-                                                       <i
-                                                            class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
-                                                       <a href="#" class="course-box__teacher-link">${course.creator}</a>
-                                                  </div>
-
-                                                  <div class="course-box__rating">
-                                                  ${Array(5 - course.courseAverageScore).fill(0).map(() =>
-                         `<img src="./images/svgs/star.svg" alt="rating"
-                                                                 class="course-box__star">`
-                    ).join('')
-                         }
-                                                       ${Array(course.courseAverageScore).fill(0).map(() =>
-                              `<img src="./images/svgs/star_fill.svg" alt="rating"
-                                                                      class="course-box__star">`
-                         ).join('')
-                         }            
-                                                  </div>
-                                             </div>
-
-                                             <div class="course-box_status d-flex justify-content-between">
-                                                  <div class="course-box__user">
-                                                       <i class="fas fa-users course-box__users-icon"></i>
-                                                       <span class="course-box__users-text">${course.registers}</span>
-                                                  </div>
-                                                  <span class="course-box__price">${course.price ? course.price.toLocaleString() : "رایگان"}</span>
-                                             </div>
-                                        </div>
-
-                                        <div class="course-box__footer mt-3">
-                                             <a href="#"
-                                                  class="course-box__footer-link d-flex align-items-center gap-2 justify-content-center py-3">
-                                                  مشاهده اطلاعات
-                                                  <i class="fas fa-arrow-left course-box__footer-icon"></i>
-                                             </a>
-                                        </div>
-                                   </div>
-                              </div>`)
-               });
-          })
+     getAndShowArticles()               
+     getAndShowNavbarMenus()
 })
